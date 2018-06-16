fee587e2a799238d0ecd0312ad745f02008bb1a5
tsukiii <archiebald.alcain@gmail.com>
2018-06-16 16:37:09 +0800

    add time info

--- src/components/info.vue
+++ src/components/info.vue
@@ -1,17 +1,23 @@
 <template>
   <div class="info-container">
     <div class="info-navbar">Info</div>
+    <div class="time-display">Today is {{ dateMessage }}<br>
+      Current time is now {{ timeMessage}}</div>
+    <div class="time-since">It's been {{ timeSinceStart }} since the project has started.</div><br>
     <div class="users-connected">There are currently {{userCount}} users connected.</div>
   </div>
 </template>
 
 <script>
-
+import moment from 'moment';
 import apiSocket from '../js/socket';
 
 export default {
   data: () => ({
     userCount: 0,
+    dateMessage: '',
+    timeMessage: '',
+    timeSinceStart: '',
   }),
   methods: {
   },
@@ -19,6 +25,9 @@ export default {
     apiSocket.socket.on('usersCount', (total) => {
       this.userCount = total;
     });
+    this.dateMessage = moment().format('MMMM Do YYYY, dddd');
+    this.timeMessage = moment().format('h:mm:ss a');
+    this.timeSinceStart = moment('20180611', 'YYYYMMDD').fromNow();
   },
 };
 </script>
